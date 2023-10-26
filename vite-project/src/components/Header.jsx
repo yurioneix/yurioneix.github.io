667f803687ee5410c46d8c8ff97bbadfcc5c584a
yurioneix <yurioneix18@gmail.com>
2023-10-26 15:39:43 -0300

    Cria componente Header e renderiza no App

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header() {
+    return ( 
+        <nav>
+            <ul>
+                <li><a href="#foto-perfil">Foto</a></li>
+                <li><a href="#sobre">Sobre mim</a></li>
+                <li><a href="#lista-habilidades">Lista de habilidades</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+     );
+}
+
+export default Header;
